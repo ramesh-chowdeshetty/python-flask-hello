4d619f871b8800ee86243de6c70a69ad382d1124
krank <karan78tp@gmail.com>
2018-06-07 18:05:56 +0530

    Sede

--- app/dokuu.py
+++ app/dokuu.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-	return "hello world updated wferf"
+	return "hello world updated 1"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=81)
